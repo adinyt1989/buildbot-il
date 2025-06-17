812018fea5386f783560e115c4cd659f2fdda966
adinyt1989 <ioffemihael@gmail.com>
2025-06-17 07:21:05 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from core.config import settings
-from bot.handlers import auth
+from bot.handlers import auth, orders, payments, profile
 
 bot = Bot(token=settings.TELEGRAM_TOKEN)
 storage = MemoryStorage()
@@ -10,9 +10,12 @@ dp = Dispatcher(bot, storage=storage)
 
 # Регистрация обработчиков
 auth.register_handlers(dp)
+orders.register_handlers(dp)
+payments.register_handlers(dp)
+profile.register_handlers(dp)
 
 async def main():
     await dp.start_polling()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
